f49e0672d96116c4a174dd2bd45f5d1c681bc076
okothpius <orukopius@gmail.com>
2021-01-19 14:50:10 +0300

    create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react"
-import logo from './logo.svg';
-import './App.css';
-
 const todoItems = [
   {
     id: 1,
@@ -49,13 +46,15 @@ class App extends Component {
   renderTabList = () => {
     return (
       <div className="my-5 tab-list">
-        <span onClick={() => this.displayCompleted(true)}
-        className={this.state.viewCompleted ? "active" : ""}
+        <span 
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
         >
           Complete
         </span>
-        <span onClick={() => this.displayCompleted(false)}
-        className={this.state.viewCompleted ? "active" : ""}
+        <span 
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
         >
           Incomplete
         </span>
@@ -68,8 +67,9 @@ class App extends Component {
       item => item.completed == viewCompleted
     );
     return newItems.map(item => (
-      <li key={item.id}
-      className="list-group-item d-flex justify-content-between align-items-center"
+      <li 
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
       >
         <span 
           className={`todo-title mr-2 ${
@@ -79,30 +79,33 @@ class App extends Component {
           >
             {item.title}
           </span>
+          <span>
+            <button className="btn btn-secondary mr-2">Edit</button>
+            <button className="btn btn-danger">Delete</button>
+          </span>
       </li>
-    ))
+    ));
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Todo App</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
